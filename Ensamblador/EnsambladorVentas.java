49ff525ac27d6db263baf979b31fbfd15ed5610f
geovana <geovanadasilvaoliveira17@gmail.com>
2024-04-19 20:05:29 +0200

    holi x3

--- Ensamblador/EnsambladorVentas.java
+++ Ensamblador/EnsambladorVentas.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class EnsambladorVentas extends Ensamblador{
     private List<Ventas> ventas;
 
-    public EnsambladorVentas(List<Cliente> clientes, List<Libros> libros, List<Archivo> archivos, List<Ventas> ventas) {
+    public EnsambladorVentas(List<Cliente> clientes, List<Libros> libros, List<Archivos> archivos, List<Ventas> ventas) {
         super(clientes, libros, archivos);
         this.ventas = ventas;
     }
--- Ensamblador/Ventas.java
+++ Ensamblador/Ventas.java
@@ -1,5 +1,4 @@
 package Ensamblador;
-
 import java.time.LocalDate;
 import java.util.List;
 
