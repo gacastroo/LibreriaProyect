2f355b673aa1df7349b377894429ca48d801f0d1
Nyx <ga.castro.2019@alumnos.urjc.es>
2024-05-16 15:39:06 +0200

    update

--- Ensamblador/Archivos/ArchivoBinario/ArchivoBinario.java
+++
@@ -1,9 +0,0 @@
-package Ensamblador.Archivos.ArchivoBinario;
-
-import Ensamblador.Archivos.Archivos;
-
-public class ArchivoBinario extends Archivos {
-    public ArchivoBinario(String nombre, String extension, String ruta) {
-        super(nombre, extension, ruta);
-    }
-}
